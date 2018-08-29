abeef2b1d329741d3f3ce370b5e778cef0023d45
YeongHanLee <good7219@hotmail.com>
2018-08-29 17:22:30 +0900

    Manifests -Schedule.java를 activity로 추가
    Java
    -Schedule.java 추가
    
    layout(xml)
    -activity_schedule.xml 추가
    
    Realm 설치 완료

---
+++ app/src/main/java/com/teamnoname/streetartzone/Schedule/Schedule.java
@@ -0,0 +1,19 @@
+package com.teamnoname.streetartzone.Schedule;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.teamnoname.streetartzone.R;
+
+/**
+ * Created by iyeonghan on 2018. 8. 29..
+ */
+
+public class Schedule extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
--- app/src/main/java/com/teamnoname/streetartzone/StreetGroup/StreetGroupAcitivty.java
+++ app/src/main/java/com/teamnoname/streetartzone/StreetGroup/StreetGroupAcitivty.java
@@ -1,4 +1,5 @@
 package com.teamnoname.streetartzone.StreetGroup;
 
 public class StreetGroupAcitivty {
+
 }
